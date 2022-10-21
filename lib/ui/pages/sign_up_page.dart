96ded707b3c8ea4b6e020c92b6d3dedb63e63c51
yovitaa <yovitaananda89@gmail.com>
2022-10-21 22:09:49 +0700

    first commit

---
+++ lib/ui/pages/sign_up_page.dart
@@ -0,0 +1,96 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widget/buttons.dart';
+import 'package:bank_sha/ui/widget/forms.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: const EdgeInsets.only(
+              top: 100,
+              bottom: 100,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                'assets/img_logo_light.png',
+                ),
+              ),
+            ),
+          ),
+          Text(
+            'Join Us to unlock\nYour Growth',
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // NOTE: NAME INPUT
+                const CustomFormField(
+                  title: 'Full Name',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                // NOTE: EMAIL INPUT
+                const CustomFormField(
+                  title: 'Email Address',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                // NOTE: PASSWORD INPUT
+                const CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Continue',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/sign-up-set-profile');
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextButton(
+            title: 'Sign In',
+            onPressed: () {
+              Navigator.pushNamed(context, '/sign-in');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

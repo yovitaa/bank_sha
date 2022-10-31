1158f0e92718d25bad0ca28dbf8a4762d609dfdd
yovitaa <yovitaananda89@gmail.com>
2022-10-31 22:21:51 +0700

    5.5 Pin Page Bag 2

--- lib/ui/pages/pin_page.dart
+++ lib/ui/pages/pin_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widget/buttons.dart';
 import 'package:flutter/material.dart';
 
 class PinPage extends StatelessWidget {
@@ -9,47 +10,122 @@ class PinPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: darkBackgroundColor,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sha PIN',
-              style: whiteTextStyle.copyWith(
-                fontSize: 20,
-                fontWeight: semiBold,
-              ),
-            ),
-            const SizedBox(
-              height: 72,
-            ),
-            SizedBox(
-              width: 200,
-              child: TextFormField(
-                obscureText: true,
-                cursorColor: greyColor,
-                obscuringCharacter: '*',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 58,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sha PIN',
                 style: whiteTextStyle.copyWith(
-                  fontSize: 36,
-                  fontWeight: medium,
-                  letterSpacing: 16,
+                  fontSize: 20,
+                  fontWeight: semiBold,
                 ),
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: greenColor,
-                    ),
+              ),
+              const SizedBox(
+                height: 72,
+              ),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  obscureText: true,
+                  cursorColor: greyColor,
+                  obscuringCharacter: '*',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 36,
+                    fontWeight: medium,
+                    letterSpacing: 16,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: greyColor,
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: greenColor,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 66,
+              ),
+              Wrap(
+                spacing: 40,
+                runSpacing: 40,
+                children: [
+                  CustomInputButton(
+                    title: '1',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '2',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '3',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '4',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '5',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '6',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '7',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '8',
+                    onTap: () {},
+                  ),
+                  CustomInputButton(
+                    title: '9',
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    width: 60,
+                    height: 60,
+                  ),
+                  CustomInputButton(
+                    title: '0',
+                    onTap: () {},
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: numberBackgroundColor,
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: whiteColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+              ),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
--- lib/ui/widget/buttons.dart
+++ lib/ui/widget/buttons.dart
@@ -54,7 +54,6 @@ class CustomTextButton extends StatelessWidget {
     this.onPressed,
   }) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -69,6 +68,41 @@ class CustomTextButton extends StatelessWidget {
           title,
           style: greyTextStyle.copyWith(
             fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomInputButton extends StatelessWidget {
+  final String title;
+  final VoidCallback? onTap;
+
+  const CustomInputButton({
+    Key? key,
+    required this.title,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: numberBackgroundColor,
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: whiteTextStyle.copyWith(
+              fontSize: 22,
+              fontWeight: semiBold,
+            ),
           ),
         ),
       ),

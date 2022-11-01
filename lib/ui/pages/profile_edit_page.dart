f636019cc2d1a7697d66c348a39e9dc055af3517
yovitaa <yovitaananda89@gmail.com>
2022-11-01 20:20:41 +0700

    5.8 Slicing Edit Profile Page

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,3 +1,6 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widget/buttons.dart';
+import 'package:bank_sha/ui/widget/forms.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditPage extends StatelessWidget {
@@ -5,6 +8,65 @@ class ProfileEditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Edit Profile',
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomFormField(
+                  title: 'Username',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormField(
+                  title: 'Full Name',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormField(
+                  title: 'Email Address',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Update Now',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/sign-up-set-profile');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

07a7ec491f82017e9bbe153e00a6e69b2e086ac0
yovitaa <yovitaananda89@gmail.com>
2022-11-03 22:42:36 +0700

    7.3 Transfer Page 3

--- lib/ui/pages/transfer_page.dart
+++ lib/ui/pages/transfer_page.dart
@@ -1,6 +1,8 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widget/buttons.dart';
 import 'package:bank_sha/ui/widget/forms.dart';
 import 'package:bank_sha/ui/widget/transfer_recent_user_item.dart';
+import 'package:bank_sha/ui/widget/transfer_result_item.dart';
 import 'package:flutter/material.dart';
 
 class TransferPage extends StatelessWidget {
@@ -36,7 +38,18 @@ class TransferPage extends StatelessWidget {
             title: 'by username',
             isShowTitle: false,
           ),
-          buildRecentUsers(),
+          // buildRecentUsers(),
+          buildResult(),
+          const SizedBox(
+            height: 274,
+          ),
+          CustomFilledButton(
+            title: 'Continue',
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
         ],
       ),
     );
@@ -75,6 +88,47 @@ class TransferPage extends StatelessWidget {
             imageUrl: 'assets/img_friend3.png',
             name: 'Rose',
             username: 'Rosie',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildResult() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Result',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Wrap(
+            spacing: 17,
+            runSpacing: 17,
+            children: const [
+              TransferResultUserItem(
+                imageUrl: 'assets/img_friend1.png',
+                name: 'Jennie',
+                username: 'jenjen',
+                isVerified: true,
+              ),
+              TransferResultUserItem(
+                imageUrl: 'assets/img_friend2.png',
+                name: 'Lisa',
+                username: 'lisaaa',
+                isVerified: true,
+                isSelected: true,
+              ),
+            ],
           ),
         ],
       ),

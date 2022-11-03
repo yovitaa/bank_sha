9662916f4c3b1149583a18af516f706075d2cd79
yovitaa <yovitaananda89@gmail.com>
2022-11-03 21:39:49 +0700

    7.2 Transfer Page 2

--- lib/ui/pages/transfer_page.dart
+++ lib/ui/pages/transfer_page.dart
@@ -1,5 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widget/forms.dart';
+import 'package:bank_sha/ui/widget/transfer_recent_user_item.dart';
 import 'package:flutter/material.dart';
 
 class TransferPage extends StatelessWidget {
@@ -34,6 +35,46 @@ class TransferPage extends StatelessWidget {
           CustomFormField(
             title: 'by username',
             isShowTitle: false,
+          ),
+          buildRecentUsers(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildRecentUsers() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recent Users',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const TransferRecentUserItem(
+            imageUrl: 'assets/img_friend1.png',
+            name: 'Jennie',
+            username: 'jenjen',
+            isVerified: true,
+          ),
+          const TransferRecentUserItem(
+            imageUrl: 'assets/img_friend2.png',
+            name: 'Lisa',
+            username: 'lisaaa',
+          ),
+          const TransferRecentUserItem(
+            imageUrl: 'assets/img_friend3.png',
+            name: 'Rose',
+            username: 'Rosie',
           ),
         ],
       ),

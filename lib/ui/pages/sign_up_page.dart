aa4a1b63389bf5ba258cec45315a0ca60183084f
yovitaa <yovitaananda89@gmail.com>
2022-11-10 22:40:57 +0700

    10.6 Implementasi SIgn Up

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,8 +1,12 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
+import 'package:bank_sha/models/sign_up_form_model.dart';
 import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/sign_up_set_profile_page.dart';
 import 'package:bank_sha/ui/widget/buttons.dart';
 import 'package:bank_sha/ui/widget/forms.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -12,112 +16,145 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-final nameController = TextEditingController(text: '');
-final emailController = TextEditingController(text: '');
-final passwordController = TextEditingController(text: '');
+  final nameController = TextEditingController(text: '');
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
 
-bool validate(){
-  if  (nameController.text.isEmpty ||
-      emailController.text.isEmpty ||
-      passwordController.text.isEmpty) {
-    return false;
+  bool validate() {
+    if (nameController.text.isEmpty ||
+        emailController.text.isEmpty ||
+        passwordController.text.isEmpty) {
+      return false;
+    }
+
+    return true;
   }
 
-  return true;
-}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          Container(
-            width: 155,
-            height: 50,
-            margin: const EdgeInsets.only(
-              top: 100,
-              bottom: 100,
-            ),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                'assets/img_logo_light.png',
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthChechEmailSuccess) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignUpSetProfilePage(
+                  data: SignUpFormModel(
+                    name: nameController.text,
+                    email: emailController.text,
+                    password: passwordController.text,
+                  ),
                 ),
               ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-          ),
-          Text(
-            'Join Us to unlock\nYour Growth',
-            style: blackTextStyle.copyWith(
-              fontSize: 20,
-              fontWeight: semiBold,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: whiteColor,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // NOTE: NAME INPUT
-                CustomFormField(
-                  title: 'Full Name',
-                  controller: nameController,
+            children: [
+              Container(
+                width: 155,
+                height: 50,
+                margin: const EdgeInsets.only(
+                  top: 100,
+                  bottom: 100,
                 ),
-                const SizedBox(
-                  height: 16,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/img_logo_light.png',
+                    ),
+                  ),
                 ),
-                // NOTE: EMAIL INPUT
-                CustomFormField(
-                  title: 'Email Address',
-                  controller: emailController,
+              ),
+              Text(
+                'Join Us to unlock\nYour Growth',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
                 ),
-                const SizedBox(
-                  height: 16,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: whiteColor,
                 ),
-                // NOTE: PASSWORD INPUT
-                CustomFormField(
-                  title: 'Password',
-                  obscureText: true,
-                  controller: passwordController,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // NOTE: NAME INPUT
+                    CustomFormField(
+                      title: 'Full Name',
+                      controller: nameController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    // NOTE: EMAIL INPUT
+                    CustomFormField(
+                      title: 'Email Address',
+                      controller: emailController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    // NOTE: PASSWORD INPUT
+                    CustomFormField(
+                      title: 'Password',
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomFilledButton(
+                      title: 'Continue',
+                      onPressed: () {
+                        if (validate()) {
+                          context
+                              .read<AuthBloc>()
+                              .add(AuthCheckEmail(emailController.text));
+                        } else {
+                          showCustomSnackbar(
+                              context, 'Semua field harus diisi');
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFilledButton(
-                  title: 'Continue',
-                  onPressed: () {
-                    if (validate()) {
-                      Navigator.pushNamed(context, '/sign-up-set-profile');
-                    } else {
-                      showCustomSnackbar(context, 'Semua field harus diisi');
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomTextButton(
-            title: 'Sign In',
-            onPressed: () {
-              Navigator.pushNamed(context, '/sign-in');
-            },
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-        ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              CustomTextButton(
+                title: 'Sign In',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/sign-in');
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/ui/pages/sign_up_set_profile_page.dart
+++ lib/ui/pages/sign_up_set_profile_page.dart
@@ -1,10 +1,16 @@
+import 'package:bank_sha/models/sign_up_form_model.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widget/buttons.dart';
 import 'package:bank_sha/ui/widget/forms.dart';
 import 'package:flutter/material.dart';
 
 class SignUpSetProfilePage extends StatelessWidget {
-  const SignUpSetProfilePage ({super.key});
+  final SignUpFormModel data;
+
+  const SignUpSetProfilePage({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {

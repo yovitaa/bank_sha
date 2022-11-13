e1a7454f92273a7e8a14f7ff977f89912f2aba41
yovitaa <yovitaananda89@gmail.com>
2022-11-13 13:12:16 +0700

    10.10 Implementasi Register

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/models/sign_up_form_model.dart';
+import 'package:bank_sha/models/user_model.dart';
 import 'package:bank_sha/services/auth_services.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
@@ -20,6 +21,18 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           } else {
             emit(const AuthFailed('Email sudah terpakai'));
           }
+        } catch (e) {
+          emit(AuthFailed(e.toString()));
+        }
+      }
+
+      if(event is AuthRegister){
+        try {
+          emit(AuthLoading());
+
+          final user = await AuthService().register(event.data);
+          
+          emit(AuthSuccess(user));
         } catch (e) {
           emit(AuthFailed(e.toString()));
         }
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:bank_sha/models/sign_up_form_model.dart';
+import 'package:bank_sha/models/user_model.dart';
 import 'package:bank_sha/shared/shared_values.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,4 +25,25 @@ class AuthService{
       rethrow;
     }
   }
+
+
+Future<UserModel> register(SignUpFormModel data) async {
+  try {
+    final res = await http.post(
+      Uri.parse('$baseUrl/register'),
+    );
+
+    if(res.statusCode == 200){
+      UserModel user = UserModel.fromJson(jsonDecode(res.body));
+      user = user.copyWith(password: data.password);
+
+      return user;
+    }
+    else {
+      throw jsonDecode(res.body)['message'];
+    }
+  } catch (e) {
+    rethrow;
+  }
+}
 }
